7c3079f7b58d1c1dc9a02844064dd27a8697e04d
Pratikbg24 <pratikghongade24@gmail.com>
2020-05-28 11:21:39 +0530

    New File

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Component/home/home.component';
+import { MyPipePipe } from './Pipe/my-pipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MyPipePipe
   ],
   imports: [
     BrowserModule,
